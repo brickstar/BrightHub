6fe226455a2bc0160228c078b3369c61294bbd1e
brickstar <brickstar@gmail.com>
2018-08-21 14:04:22 -0600

    add avatar url to users and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
 
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.provider = auth[:provider]
-      user.uid = auth[:uid]
-      user.name = auth[:info][:name]
-      auth[:extra][:raw_info][:avatar_url]
+      user.provider   = auth[:provider]
+      user.uid        = auth[:uid]
+      user.name       = auth[:info][:name]
+      user.avatar_url = auth[:extra][:raw_info][:avatar_url]
     end
   end
 
