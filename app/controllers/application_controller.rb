47f65aabba8ba1842cf565d0749972065121d031
brickstar <brickstar@gmail.com>
2018-08-22 12:21:48 -0600

    update conn method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :conn
+
+  def conn
+    Faraday.new("https://api.github.com") do |faraday|
+      faraday.headers["Authorization"] = "token #{current_user.token}"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
